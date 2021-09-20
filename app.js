b9a04e8731a5c5f26d546ef171e9ea6d626fb65e
dc <enzodc6688@gmail.com>
2021-09-20 22:11:31 +0200

    change

--- app.js
+++ app.js
@@ -5,20 +5,23 @@ const morgan = require('morgan');
 const index = require('./routes/index');
 const mail = require('./routes/mail');
 const fs = require('fs');
-const port = 3000 || process.env.PORT ;
+const port = 3000 || process.env.PORT;
 
 require("dotenv").config();
 
-app.set('views',path.join(__dirname,'views'));
-app.use(express.static(path.join(__dirname,'/public')));
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('tiny'));
 
 app.use(index);
 app.use(mail);
 module.exports = app;
 app.listen(port)
+
+
+console.log("bienvenue sur le port : www.localhost:3000", port);
