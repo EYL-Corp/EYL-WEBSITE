a6cc948324d53776cf490b1a38ebe9a324162cfc
dc <enzodc6688@gmail.com>
2021-09-24 16:17:00 +0200

    some modif

--- routes/index.js
+++ routes/index.js
@@ -5,21 +5,22 @@ router.get('/', (req, res) => {
     res.render('index');
 })
 
-router.get('/home',(req,res) => {
+router.get('/home', (req, res) => {
     res.render('home');
 })
 
-router.get('/tarifs',(req,res) => {
+router.get('/tarifs', (req, res) => {
     res.render('tarifs');
 })
 
-router.get('/contact',(req,res) => {
+router.get('/contact', (req, res) => {
     res.render('contact');
 })
 
-router.get('/equipe',(req,res) => {
+router.get('/equipe', (req, res) => {
     res.render('equipe');
 })
+
 
 
 module.exports = router;
