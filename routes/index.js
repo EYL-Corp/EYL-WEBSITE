570eb049246474c27c670bde7f347725336593d0
swi <enzodc6688@gmail.com>
2022-03-26 22:45:49 +0100

    vuln

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ router.get('/', (req, res) => {
     res.render('index');
 })
 
+router.get('/nav', (req, res) => {
+    res.render('nav');
+})
+
 router.get('/home', (req, res) => {
     res.render('home');
 })
